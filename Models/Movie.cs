93d74ffd192973be170fee229134b32378b3e1ef
Theodor Nordkvist <teddin2@outlook.com>
2021-05-26 00:03:16 +0200

    Create Movies table

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace MvcMovieIdentity.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public int ReleaseYear { get; set; }
+        [Required]
         public int GenreId { get; set; }
     }
 }
